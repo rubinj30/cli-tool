992128ae8d9cc5a8f4fd52fe1c8a62944ca7fa35
Jonathan Rubin <h356393@MacBook-Pro.local>
2019-04-27 10:12:17 -0400

    Fixed script so working w/ Generate, cleaned up comments

--- bin/index.js
+++ bin/index.js
@@ -1,22 +1,19 @@
 #!/usr/bin/env node
-
 const forge = require('../lib/forge')
 
-// TODO: find out why this is different and best way
-// const createStream = fs.createWriteStream('test.txt');
-// createStream.end();
 
-// TODO:
-// 1)  process.argv[1] gets location of file ran (index.js) -> may need this at some point to navigate from here to another location
-// 2) should an arg be passed like "generate" and "component"
-
-// gets component name from command line argument
-const componentName = forge.uppercase(process.argv[2]);
-const test = async () => {
-    forge.createDirectory(componentName);
-    forge.createComponent(componentName)
-    forge.createTest(componentName)
-    forge.createSassFile(componentName)
+const createDirectoryWithFiles = (componentName) => {
+    const name = forge.uppercaseFirstLetter(componentName)
+    forge.createDirectory(name);
+    forge.createComponent(name)
+    forge.createTest(name)
+    forge.createSassFile(name)
 }
 
-test();
+// gets specific action and component name from arguments
+const [, , action, componentName] = process.argv;
+
+// if the action is "generate", it will generate a component file
+if (action.toLowerCase() === 'generate' || action.toLowerCase() === 'g') {
+    createDirectoryWithFiles(componentName);
+}
